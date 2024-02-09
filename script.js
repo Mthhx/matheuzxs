289f788acc52f847a6b0d10b7cba6e907b0a6345
Mthhx <142568784+Mthhx@users.noreply.github.com>
2024-02-08 22:25:02 -0300

    meu site

---
+++ script.js
@@ -0,0 +1,13 @@
+// Função para ser executada quando o documento HTML estiver completamente carregado
+document.addEventListener("DOMContentLoaded", function() {
+  // Selecionando o botão pelo ID e adicionando um evento de clique
+  var meuBotao = document.getElementById("meuBotao");
+  if(meuBotao){
+      meuBotao.onclick = function() {
+        // Abrir o link do site quando o botão for clicado
+        window.open("https://pt.pornhub.com/", "_blank");
+      };
+  } else {
+      console.error("Elemento com ID 'meuBotao' não encontrado.");
+  }
+});
